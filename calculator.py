bf8be1112bdf8f0dea301d51bd94c00f9d14da1d
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2026-01-17 02:13:58 +0000

    add calculator function that handles all operators

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculate(a, op, b):
+    if op == "+":
+        return a + b
+    elif op == "-":
+        return a - b
+    elif op == "*":
+        return a * b
+    elif op == "/":
+        if b == 0:
+            return "cannot divide by zero"
+        return a / b
