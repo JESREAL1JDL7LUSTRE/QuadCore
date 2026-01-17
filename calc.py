c62f59ab59818027a403800b850876ae03c3fead
Bea Clarise Bacaling <bacaling.beaclarise@gmail.com>
2026-01-17 02:56:16 +0000

    added subtraction function

--- calc.py
+++ calc.py
@@ -1,25 +1,30 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+    
+    def subtract(self, a, b):
+        return a - b
 
 # Instantiate the class
 calc = Calculator()
 
 while True:
-    print("\n1-add")
+    print("\n1-add, 2-subtract")
     choice = input("function: ")
 
     if choice == '5':
         print("Exiting calculator. Goodbye!")
         break
 
-    if choice in ('1'):
+    if choice in ('1', '2'):
         try:
             num1 = float(input("select number 1: "))
             num2 = float(input("select number 2: "))
 
             if choice == '1':
                 print(f"Sum: {calc.add(num1, num2)}")
+            elif choice == '2':
+                print(f"Difference: {calc.subtract(num1, num2)}")
         except ValueError:
             print("Invalid input! Please enter numerical values.")
     else:
