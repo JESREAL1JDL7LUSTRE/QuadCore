5139dc8f8fcc289e0fe953d1aaa21ed69f397ca8
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2026-01-17 02:43:47 +0000

    added division function and exit

--- calculator.py
+++ calculator.py
@@ -8,21 +8,26 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
 
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero!"
+        return a/b
+
 # Instantiate the class
 calc = Calculator()
 
 while True:
-    print("\n1-add, 2-substract, 3-multiply")
+    print("\n1-Add, 2-Substract, 3-Multiply, 4-Divide, 5-Exit")
     choice = input("Select Operator: ")
 
     if choice == '5':
         print("Exiting calculator. Goodbye!")
         break
 
-    if choice in ('1', '2', '3'):
+    if choice in ('1', '2', '3', '4', '5'):
         try:
-            num1 = float(input("select number 1: "))
-            num2 = float(input("select number 2: "))
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
             if choice == '1':
                 print(f"Sum: {calc.add(num1, num2)}")
@@ -30,6 +35,8 @@ while True:
                 print(f"Difference: {calc.substract(num1, num2)}")
             elif choice == '3':
                 print(f"Product: {calc.multiply(num1, num2)}")
+            elif choice == '4':
+                print(f"Quotient: {calc.divide(num1, num2)}")
         except ValueError:
             print("Invalid input! Please enter numerical values.")
     else:
