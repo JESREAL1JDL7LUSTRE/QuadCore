df9ee5099082f82e8a895d02348930d02d47aedd
Jesreal D. Lustre <115926421+JESREAL1JDL7LUSTRE@users.noreply.github.com>
2026-01-17 02:36:44 +0000

    added multiplication operation

--- calculator.py
+++ calculator.py
@@ -4,19 +4,22 @@ class Calculator:
     
     def subtract(self, a, b):
         return a - b
+    
+    def multiply(self, a, b):
+        return a * b
 
 # Instantiate the class
 calc = Calculator()
 
 while True:
-    print("\n1-add, 2-substract ")
-    choice = input("function: ")
+    print("\n1-add, 2-substract, 3-multiply")
+    choice = input("Select Operator: ")
 
     if choice == '5':
         print("Exiting calculator. Goodbye!")
         break
 
-    if choice in ('1'):
+    if choice in ('1', '2', '3'):
         try:
             num1 = float(input("select number 1: "))
             num2 = float(input("select number 2: "))
@@ -25,6 +28,8 @@ while True:
                 print(f"Sum: {calc.add(num1, num2)}")
             elif choice == '2':
                 print(f"Difference: {calc.substract(num1, num2)}")
+            elif choice == '3':
+                print(f"Product: {calc.multiply(num1, num2)}")
         except ValueError:
             print("Invalid input! Please enter numerical values.")
     else:
