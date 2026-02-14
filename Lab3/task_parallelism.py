852aed1d57fce068dc6703df231018326642d24b
Jesreal D. Lustre <jesreal.lustre@yahoo.com>
2026-02-14 00:29:20 +0000

    coded the prints

--- Lab3/task_parallelism.py
+++ Lab3/task_parallelism.py
@@ -22,3 +22,26 @@ employees = [
     ("Diana", 40000),
     ("Edward", 35000)
 ]
+
+
+for name, salary in employees:
+    with ThreadPoolExecutor() as executor:
+        future_sss = executor.submit(compute_sss, salary)
+        future_ph = executor.submit(compute_philhealth, salary)
+        future_pg = executor.submit(compute_pagibig, salary)
+        future_tax = executor.submit(compute_tax, salary)
+
+        sss = future_sss.result()
+        philhealth = future_ph.result()
+        pagibig = future_pg.result()
+        tax = future_tax.result()
+
+    total_deduction = sss + philhealth + pagibig + tax
+
+    print(f"\nEmployee: {name}")
+    print(f"Gross Salary: {salary}")
+    print(f"SSS: {sss:.2f}")
+    print(f"PhilHealth: {philhealth:.2f}")
+    print(f"Pag-IBIG: {pagibig:.2f}")
+    print(f"Tax: {tax:.2f}")
+    print(f"Total Deduction: {total_deduction:.2f}")
