baf1e9b6717df48101169610dae5a25ac9e856d6
Gil John Rey A. Naldoza <naldoza.gil30@gmail.com>
2026-02-14 00:37:54 +0000

    finished function

--- Lab3/data_parallelism.py
+++ Lab3/data_parallelism.py
@@ -13,3 +13,14 @@ def compute_total_deduction(employee):
     total = salary * (0.045 + 0.025 + 0.02 + 0.10)
     net_salary = salary - total
     return name, salary, total, net_salary
+
+
+if __name__ == "__main__":
+    with ProcessPoolExecutor() as executor:
+        results = executor.map(compute_total_deduction, employees)
+
+    for name, salary, total, net in results:
+        print(f"\nEmployee: {name}")
+        print(f"Gross Salary: {salary}")
+        print(f"Total Deduction: {total:.2f}")
+        print(f"Net Salary: {net:.2f}")
