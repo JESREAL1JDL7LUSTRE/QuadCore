a7befde17a38c68a4357b9bdbba7c639492b4808
Jesreal D. Lustre <jesreal.lustre@yahoo.com>
2026-04-18 08:29:40 +0000

    initial test function added

--- Sequential_vs_Parallel/main.py
+++ Sequential_vs_Parallel/main.py
@@ -14,3 +14,24 @@ def time_function(func, *args):
     result = func(*args)
     end = time.time()
     return result, end - start
+
+def run_test(size):
+    print("\n" + "=" * 60)
+    print(f"DATASET SIZE: {size}")
+    print("=" * 60)
+
+    data = generate_data(size)
+
+    target = data[size // 2]
+
+    arr1 = data.copy()
+    _, seq_time = time_function(mergeSort, arr1, 0, len(arr1) - 1)
+
+    arr2 = data.copy()
+    par_result, par_time = time_function(parallelMergeSort, arr2)
+
+    arr3 = data.copy()
+    _, seq_search_time = time_function(linearSearch, arr3, target)
+
+    arr4 = data.copy()
+    _, par_search_time = time_function(parallel_linear_search, arr4, target)
