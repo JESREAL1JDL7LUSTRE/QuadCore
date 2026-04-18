518f43d33f5dc1b26dc401dc0a9044d4567774b0
Angel Janette D. Taglucop <angeltaglucop@gmail.com>
2026-04-18 08:28:14 +0000

    add generate dataset and time function

--- Sequential_vs_Parallel/main.py
+++ Sequential_vs_Parallel/main.py
@@ -6,3 +6,11 @@ from parallelSortingAlgorithm import parallelMergeSort
 from sequentialSearchingAlgorithm import linearSearch
 from parallelSearchingAlgorithm import parallel_linear_search
 
+def generate_data(n):
+    return [random.randint(1, 1000000) for _ in range(n)]
+
+def time_function(func, *args):
+    start = time.time()
+    result = func(*args)
+    end = time.time()
+    return result, end - start
